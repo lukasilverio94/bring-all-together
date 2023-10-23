eaf509b77a77bf6dacc6e7fb399b1721a24d0474
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-24 00:00:53 +0200

    added suggestion button functionality

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const form = document.getElementById("form");
 const signInFeedBackBtn = document.getElementById("sign-in-feedback");
 //get tag to change text
 const signInFeedBackText = document.querySelector(".sign-in-text");
-
 //Elements to add content by user:
 //get input to add content (type text)
 const inputEl = document.getElementById("input-add-content");
 //get button to add content
 const buttonAddEl = document.querySelector(".add-content-btn");
 //get ul element to render
-const ul = document.getElementById("list-content-user");
-
+const ulList = document.getElementById("list-content-user");
+//get msg element, in case of dont type anything get a error:
+const msg = document.querySelector(".msg-error");
+//get clear button
+const clearBtn = document.querySelector("#clear-btn");
 
 //Add an event listener to the forms submit button
 form.addEventListener("submit", submitEvent);
@@ -30,5 +32,24 @@ function submitEvent(event) {
   signInFeedBackText.textContent = `You choose to become a ${selectedValue}. Good Luck at the journey!`;
 }
 
-
 //Add content by user:
+buttonAddEl.addEventListener("click", () => {
+  if (inputEl.value === "" || inputEl.value.length < 2) {
+    msg.classList.add("msg-error");
+    msg.innerHTML = "Please enter some valid content...";
+    setTimeout(() => msg.remove(), 2000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${inputEl.value}`));
+    ulList.appendChild(li);
+  }
+});
+
+//clear field
+clearBtn.addEventListener("click", clearField);
+
+function clearField() {
+  inputEl.value = "";
+  ulList.innerHTML = "";
+  console.log("cleaning");
+}
